ea3eeacbfbd15a7936b3897d8f746c310043cee7
NAVEEN <pnk0006@gmail.com>
2024-11-07 17:27:57 +0530

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ inputCount }}</h1>
+    <button @click="decrement">-</button>
+    <input type="number" v-model="inputCount" @input="updateCount" />
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+export default {
+  name: "HelloWorld",
+  computed: {
+    ...mapGetters(["getCount"]),
+    inputCount: {
+      get() {
+        return this.getCount;
+      },
+      set(value) {
+        this.setCount(Number(value)); // update store count based on input
+      },
+    },
+  },
+  methods: {
+    ...mapActions(["increment", "decrement", "setCount"]),
+    updateCount(event) {
+      this.setCount(Number(event.target.value)); // update store count based on input change
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/store.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    },
+  },
+  actions: {
+    increment({ commit }) {
+      commit("increment");
+    },
+    decrement({ commit }) {
+      commit("decrement");
+    },
+  },
+  getters: {
+    getCount: (state) => state.count,
+  },
+});
